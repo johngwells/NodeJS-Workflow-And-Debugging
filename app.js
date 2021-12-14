a15d8e15f77941dc16f05e8c1b93044c237d6c5a
John Wells <molekewl2017@gmail.com>
2021-12-14 01:05:26 -0800

    use express js / get & post requrest / middleware / & serve html documents

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const routes = require('./routes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded());
 
-const server = http.createServer(routes.handler);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+});
+
+app.listen(3000);
